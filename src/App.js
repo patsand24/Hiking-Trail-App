a1de6460c56806420e88b4c089f353beaab02d53
Patrick Sandoval <patsand24csu@yahoo.com>
2017-12-05 15:30:12 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1 className="App-title">Seattle Hiking Trails</h1>
+        <p className="App-intro">
+         Hiking Trails in Seattle and Puget Sound Region.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
